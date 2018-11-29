c08de4761dc0760f41078335e1df50bdcc92eb6a
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2018-11-29 19:01:40 +0100

    Created first test

--- Django_testing/dj/dj/views.py
+++ Django_testing/dj/dj/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.views import View
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('result')
+        return HttpResponse('Hello world')
 
