72ac9186a71be32a6be467fc527d183f8d0a5359
Lorenzo Bugli <bugli.lorenzo@gmail.com>
2019-04-01 04:35:01 +0200

    Function move, inline variable

--- Martin_Fowler/example1/problem.py
+++ Martin_Fowler/example1/problem.py
@@ -1,46 +1,38 @@
 import json
 
-with open("example1/plays.json", 'r') as plays_file:
-    plays = json.load(plays_file)
-
-
 def statement(invoice, plays):
+    def playFor(aPerformance):
+        return plays[aPerformance['playID']]
+
+    def amountFor(aPerformance):
+        result = 0
+        if playFor(aPerformance)['type'] == "tragedy":
+            result = 40000
+            if aPerformance['audience'] > 30:
+                result += 1000 * (aPerformance['audience'] - 30)
+        elif playFor(aPerformance)['type'] == "comedy":
+            result = 30000
+            if aPerformance['audience'] > 20:
+                result += 10000 + 500 * (aPerformance['audience'] - 20)
+            result += 300 * aPerformance['audience']
+        else:
+            raise Exception(f"uknown type: {playFor(aPerformance)['type']}")
+        return result
+
     total_amount = 0
     volume_credits = 0
     result = f"Statement for {invoice['customer']}\n"
     format = '${:,.2f}'
 
     for perf in invoice['performances']:
-        this_amount = amountFor(perf)
-
         volume_credits += max(perf['audience'] - 30, 0)
         if "comedy" == playFor(perf)['type']: volume_credits += round(perf['audience'] / 5)
 
-        result += f"    {playFor(perf)['name']}: {format.format(this_amount/100)} ({perf['audience']} seats)\n"
-        total_amount += this_amount
+        result += f"    {playFor(perf)['name']}: {format.format(amountFor(perf)/100)} ({perf['audience']} seats)\n"
+        total_amount += amountFor(perf)
 
     result += f"Amount owed is ({format.format(total_amount/100)})\n"
     result += f"You earned {volume_credits} credits\n"
-    return result
-
-
-def playFor(aPerformance):
-    return plays[aPerformance['playID']]
-
-
-def amountFor(aPerformance):
-    result = 0
-    if playFor(aPerformance)['type'] == "tragedy":
-        result = 40000
-        if aPerformance['audience'] > 30:
-            result += 1000 * (aPerformance['audience'] - 30)
-    elif playFor(aPerformance)['type'] == "comedy":
-        result = 30000
-        if aPerformance['audience'] > 20:
-            result += 10000 + 500 * (aPerformance['audience'] - 20)
-        result += 300 * aPerformance['audience']
-    else:
-        raise Exception(f"uknown type: {playFor(aPerformance)['type']}")
     return result
 
 
