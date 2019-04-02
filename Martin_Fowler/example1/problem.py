288466b267d62ab0194049dc778bef9ce3edf6bb
Lorenzo Bugli <bugli.lorenzo@gmail.com>
2019-04-02 03:30:46 +0200

    Move function

--- Martin_Fowler/example1/problem.py
+++ Martin_Fowler/example1/problem.py
@@ -1,5 +1,6 @@
-import copy
 import json
+
+from .model import createStatementData
 
 
 def statement(invoice, plays):
@@ -14,58 +15,9 @@ def statement(invoice, plays):
         result += f"You earned {statement_data.total_volume_credits} credits\n"
         return result
 
-    def createStatementData(invoice, plays):
-        class Performance(object):
-            def __init__(self, aPerformance):
-                self._performance = copy.deepcopy(aPerformance)
-                self.play = self.playFor()
-                self.amount = self.amountFor()
-                self.credits = self.volumeCreditsFor()
-
-            def playFor(self):
-                return plays[self['playID']]
-
-            def amountFor(self):
-                if self.play['type'] == "tragedy":
-                    result = 40000
-                    if self['audience'] > 30:
-                        result += 1000 * (self['audience'] - 30)
-                elif self.play['type'] == "comedy":
-                    result = 30000
-                    if self['audience'] > 20:
-                        result += 10000 + 500 * (self['audience'] - 20)
-                    result += 300 * self['audience']
-                else:
-                    raise Exception(f"uknown type: {self.play['type']}")
-                return result
-
-            def volumeCreditsFor(self):
-                result = max(self['audience'] - 30, 0)
-                if "comedy" == self.play['type']: result += round(self['audience'] / 5)
-                return result
-
-            def __getitem__(self, item):
-                return self._performance[item]
-
-        class StatementData(object):
-            def __init__(self, customer, performances):
-                self.customer = customer
-                self.performances = performances
-                self.total_amount = self.totalAmount(performances)
-                self.total_volume_credits = self.totalVolumeCredits(performances)
-
-            def totalAmount(self, performances):
-                return sum([p.amount for p in performances])
-
-            def totalVolumeCredits(self, performances):
-                return sum([p.credits for p in performances])
-
-        return StatementData(
-            customer=invoice['customer'],
-            performances=[Performance(p) for p in invoice['performances']]
-        )
-
-    return renderPlainText(createStatementData(invoice, plays))
+    return renderPlainText(
+        createStatementData(invoice, plays)
+    )
 
 
 if __name__ == "__main__":
