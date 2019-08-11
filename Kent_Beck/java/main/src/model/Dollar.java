78f4fca60fc73249005459d710eda2508a4feab8
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-08-11 20:04:19 +0200

    First test

--- Kent_Beck/java/main/src/model/Dollar.java
+++ Kent_Beck/java/main/src/model/Dollar.java
@@ -1,4 +1,13 @@
 package model;
 
 public class Dollar {
+    public double amount;
+
+    public Dollar(double amount) {
+        this.amount = amount;
+    }
+
+    public void times(int value) {
+        this.amount *= value;
+    }
 }
--- Kent_Beck/java/main/src/tests/DollarTest.java
+++ Kent_Beck/java/main/src/tests/DollarTest.java
@@ -1,13 +1,18 @@
 package tests;
 
 // using Junit5.4
+
+import model.Dollar;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class DollarTest {
 
     @Test
-    public void given_5_Dollar_when_multiplied_by_2_than_result_10(){
-
+    public void given_5_Dollar_when_multiplied_by_2_than_result_10() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
     }
 }
