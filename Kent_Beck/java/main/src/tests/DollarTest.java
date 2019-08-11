0d7bab5836a5dd05a3ce4afb9e85a716ccd66b12
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-08-11 19:53:50 +0200

    Starting code

---
+++ Kent_Beck/java/main/src/tests/DollarTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+// using Junit5.4
+import org.junit.jupiter.api.Test;
+
+
+public class DollarTest {
+
+    @Test
+    public void given_5_Dollar_when_multiplied_by_2_than_result_10(){
+
+    }
+}
