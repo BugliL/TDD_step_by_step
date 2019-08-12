1f384d7c9fdced07e757be8d464028725bfef4c0
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-08-12 19:17:04 +0200

    CODE: franc, equality

--- Kent_Beck/java/main/src/model/Franc.java
+++ Kent_Beck/java/main/src/model/Franc.java
@@ -6,4 +6,19 @@ public class Franc {
     public Franc(double amount) {
         this.amount = amount;
     }
+
+    public Franc times(int multiplier) {
+        return new Franc(this.amount * multiplier);
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Franc franc_obj = (Franc) obj;
+        return franc_obj.amount == this.amount;
+    }
 }
