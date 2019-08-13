a9ac0d4ba1555ce5b8de9728774dd91a74c3be29
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-08-13 15:52:24 +0200

    TEST: added money test

--- Kent_Beck/java/main/src/tests/MoneyTest.java
+++ Kent_Beck/java/main/src/tests/MoneyTest.java
@@ -1,4 +1,21 @@
 package tests;
 
+import model.Dollar;
+import model.Franc;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@DisplayName("Given dollars and francs")
 public class MoneyTest {
+
+    @Test
+    @DisplayName("when compared 5USD and 5UHD result different")
+    public void test_money_equality() {
+        Dollar dollar = new Dollar(5);
+        Franc franc = new Franc(5);
+        assertNotEquals(dollar, franc);
+    }
 }
