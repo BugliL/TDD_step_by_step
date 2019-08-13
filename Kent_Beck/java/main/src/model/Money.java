e586568baf01addebaead4b1efaba142e670ac93
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-08-13 15:33:29 +0200

    CODE: added Money equals method

--- Kent_Beck/java/main/src/model/Money.java
+++ Kent_Beck/java/main/src/model/Money.java
@@ -2,4 +2,13 @@ package model;
 
 public abstract class Money {
     protected double amount;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+
+        Money money = (Money) obj;
+        return money.amount == this.amount;
+    }
 }
