d5e76341879c7c22f424986a86c99469ad37ed8c
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-11-21 09:53:09 +0100

    Refactoring board methods

--- Game of life/tell_dont_ask/board.py
+++ Game of life/tell_dont_ask/board.py
@@ -9,28 +9,23 @@ class Board(object):
         self.cells = cells
 
     def evolve(self):
-        # This is the most modified method in the whole program
+        # Created 2 functions to wrap behavior
 
+        self._set_future_states()
+        self._evolve_cells()
+
+    def _evolve_cells(self):
+        for cell in self.cells:
+            cell.evolve()
+
+    def _set_future_states(self):
         for cell in self.cells:
             n = len([c for c in cell.neighbours if c.is_alive])
             if n < 2:
-                # Old version
-                # cell.future_state = Cell.DeadStatus
                 cell.die_in_future()
             elif n == 2:
-                # Old version
-                # cell.future_state = cell.is_alive
                 cell.stay_in_future()
             elif n == 3:
-                # Old version
-                # cell.future_state = Cell.AliveStatus
                 cell.be_alive_in_future()
             elif n > 3:
-                # Old version
-                # cell.future_state = Cell.DeadStatus
                 cell.die_in_future()
-
-        for cell in self.cells:
-            # Old version
-            # cell.is_alive = cell.future_state
-            cell.evolve()
