0aeff208fc90ebabde92195aa1f5edb4b76394eb
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-11-26 10:04:52 +0100

    Removed method setting neighbours

--- Game of life/immutability/cell.py
+++ Game of life/immutability/cell.py
@@ -6,12 +6,10 @@ class Cell(object):
     DeadStatus = False
 
     def __init__(self, status: bool, neighbours: Iterable):
-        self._set_neighbours(neighbours)
-        self.is_alive = status
-        self.future_state = False
-
-    def _set_neighbours(self, neighbours):
         self.neighbours = neighbours
         for cell in self.neighbours:
             if self not in cell.neighbours:
                 cell.neighbours.append(self)
+
+        self.is_alive = status
+        self.future_state = False
