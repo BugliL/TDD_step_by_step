de418f00f15c144cd49a1a0d8cda83a70e878d69
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-12-21 00:00:02 +0100

    Updated example with another condition

--- Refactoring by Martin_Fowler/Theory/Replace nested conditional with guard clauses/README.md
+++ Refactoring by Martin_Fowler/Theory/Replace nested conditional with guard clauses/README.md
@@ -1,13 +1,10 @@
 # Replace nested conditional with guard clause
 A *guard clause* is an if that returns directly a value breaking function execution.  
 This pattern consist in replacing each if/else condition with a *guard clause*.  
-This pattern is made to make code clearer.
-
-![Schema](./image.png)
- 
-Shortly: < Short version >
+This pattern is made to make code clearer on each case.
 
 ## How to Replace nested conditional with guard clause
+
  **Example**
  ```python
  < Source code >   
--- Refactoring by Martin_Fowler/Theory/Replace nested conditional with guard clauses/example.py
+++ Refactoring by Martin_Fowler/Theory/Replace nested conditional with guard clauses/example.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 class Employee():
     is_separated: bool
     is_retired: bool
+    age: int
 
 
 def calculate_amount(employee):
@@ -14,12 +15,24 @@ def calculate_amount(employee):
 
 def pay_amount(employee: Employee):
     if employee.is_separated:
-        result = {'amount':0, 'reason_code': "SEP"}
+        result = {'amount': 0, 'reason_code': "SEP"}
     else:
         if employee.is_retired:
-            result = {'amount':0, 'reason_code': "RET"}
+            if employee.age < 60:
+                result = {'amount': calculate_amount(employee), 'reason_code': "A_REASON"}
+            else:
+                result = {'amount': 0, 'reason_code': "RET"}
         else:
-            result = {'amount':calculate_amount(employee), 'reason_code': "A_REASON"}
+            result = {'amount': calculate_amount(employee), 'reason_code': "A_REASON"}
 
     return result
 
+
+if __name__ == '__main__':
+    employee = Employee(
+        is_separated=False,
+        is_retired=True,
+        age=67
+    )
+
+    print(pay_amount(employee))
