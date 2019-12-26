82f8cc062477541e6b1812125c80dbaeb7ecd23b
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-12-26 01:47:58 +0100

    Replace with function call

--- Refactoring by Martin_Fowler/Theory/Encapsulate variable/final_result.py
+++ Refactoring by Martin_Fowler/Theory/Encapsulate variable/final_result.py
@@ -1,7 +1,10 @@
 default_owner = {'first_name': 'John', 'last_name': 'Smith'}
 
-spaceship = {'owner': default_owner}
-
 
 def get_default_owner():
     return default_owner
+
+
+# Replace with function call
+spaceship = {'owner': get_default_owner()}
+
