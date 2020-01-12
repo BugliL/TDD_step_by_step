145f22653a88f14966375213feb826f051f71527
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2020-01-13 00:54:50 +0100

    Semantic refactorings

--- Refactoring by Martin_Fowler/Theory/Replace primitive with object/example.py
+++ Refactoring by Martin_Fowler/Theory/Replace primitive with object/example.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from datetime import datetime
+from typing import Union
 
 
 @dataclass
@@ -9,13 +10,16 @@ class Priority:
     def __str__(self) -> str:
         return self.value
 
+    def __post_init__(self):
+        if self.value not in ['high', 'low']:
+            raise ValueError("{} is not a valid priority".format(self.value))
 
 @dataclass
 class Order:
     ID: int
     date: datetime
     description: str
-    priority: str
+    priority: Union[str, Priority]
 
     _priority: Priority = field(init=False, repr=False)
 
@@ -24,8 +28,8 @@ class Order:
         return self._priority
 
     @priority.setter
-    def priority(self, value):
-        self._priority = Priority(value)
+    def priority(self, value: Union[str, Priority]):
+        self._priority = Priority(value) if type(value) != Priority else value
 
 
 if __name__ == '__main__':
@@ -45,7 +49,9 @@ if __name__ == '__main__':
     ]
 
     high_priority = Priority('high')
+    low_priority = Priority('low')
     orders[0].priority = 'high'
+    orders[0].priority = high_priority
 
     filtered_orders = list(filter(lambda x: x.priority == high_priority, orders))
     assert len(filtered_orders) == 1
