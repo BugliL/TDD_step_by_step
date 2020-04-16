2e1e862b2f8e4cd9ca091b0828b4eb73cac46fb0
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2020-04-17 01:23:36 +0200

    Added notes and private fields

--- DDD by Alexey_Zimarev/domain/classified_ad.py
+++ DDD by Alexey_Zimarev/domain/classified_ad.py
@@ -1,17 +1,30 @@
-import decimal
-import uuid
+from decimal import Decimal
+from uuid import UUID
 
 
 class ClassifiedAd:
 
-    def __init__(self, id: uuid.UUID):
-        self.id: uuid.UUID = id
+    def __init__(self, id: UUID):
+        # in the book version here there's a check
+        # if (id == default)
+        #   throw new ArgumentException( "Identity must be specified", nameof(id));
+        # this is not pythonic and I am not checking that the parameter is passed
 
-        self.ownerId: uuid.UUID = None
-        self.title: str = None
-        self.text: str = None
-        self.price: decimal.Decimal = None
+        # add private things
+        self.__id: UUID = id
+
+        self.__ownerId: UUID = None
+        self.__title: str = None
+        self.__text: str = None
+        self.__price: Decimal = None
+
+    @property
+    def id(self):
+        return self.__id
 
 
 if __name__ == '__main__':
+    import uuid
+
     x = ClassifiedAd(id=uuid.uuid4())
+    print(x.id)
