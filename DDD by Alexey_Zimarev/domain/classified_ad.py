049065e831bb2dde5f3ae791c4ac123f5920b892
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2020-04-18 18:58:16 +0200

    Added status at ClassifiedAd

--- DDD by Alexey_Zimarev/domain/classified_ad.py
+++ DDD by Alexey_Zimarev/domain/classified_ad.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from decimal import Decimal
+from enum import Enum
 from uuid import UUID
 
 from domain.classified_ad_id import ClassifiedAdId
@@ -8,25 +10,61 @@ from domain.price import Price
 from domain.user_id import UserId
 
 
-class ClassifiedAd:
+class ClassifiedAdStatus(Enum):
+    Inactive = 'INACTIVE'
+    PendingReview = 'PENDINGREVIEW'
+    Active = 'ACTIVE'
+    MarkedAsSold = 'MARKEDASSOLD'
 
+
+class ClassifiedAd:
     def __init__(self, id: UUID, owner_id: UUID):
         self.__id: ClassifiedAdId = ClassifiedAdId(id)
         self.__owner_id: UserId = UserId(owner_id)
 
+        self.__status: ClassifiedAdStatus = ClassifiedAdStatus.Inactive
         self.__title: ClassifiedAdTitle = None
         self.__text: ClassifiedAdText = None
         self.__price: Price = None
+        self.__approved_by: UserId = None
+
+        self.__post_init__()
 
     def set_title(self, title: str):
         self.__title = title
+        self.__post_init__()
 
     def update_text(self, text: str):
         self.__text = text
+        self.__post_init__()
 
     def update_price(self, price: Decimal):
         self.__price = price
+        self.__post_init__()
+
+    def request_to_publish(self):
+        self.__status = ClassifiedAdStatus.PendingReview
+        self.__post_init__()
+
+    def __post_init__(self):
+        if (not self.__id) or (not self.__owner_id) or (not self.__status):
+            raise ValueError("Invalid state")
+
+        if self.__status == ClassifiedAdStatus.PendingReview:
+            if not all([self.__text, self.__title, self.__price]):
+                raise ValueError("Invalid state")
+
+        if self.__status == ClassifiedAdStatus.Active:
+            if not all([self.__text, self.__title, self.__price, self.__approved_by]):
+                raise ValueError("Invalid state")
+
+        if self.__status == ClassifiedAdStatus.Inactive:
+            return
 
     @property
     def id(self) -> ClassifiedAdId:
         return self.__id
+
+    @property
+    def status(self) -> ClassifiedAdStatus:
+        return self.__status
---
+++ DDD by Alexey_Zimarev/domain/tests/classified_ad_tests.py
@@ -0,0 +1,65 @@
+import unittest
+
+import uuid
+
+from domain.classified_ad import ClassifiedAd, ClassifiedAdStatus
+from domain.classified_ad_text import ClassifiedAdText
+from domain.classified_ad_title import ClassifiedAdTitle
+from domain.tests.money_tests import FakeCurrencyLookup
+from domain.price import Price
+
+
+# noinspection PyPep8Naming
+class ClassifiedAdTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.params = {'id': uuid.uuid4(), 'owner_id': uuid.uuid4()}
+
+    def test_given_created_classified_ad_should_no_error_after_creation(self):
+        x = ClassifiedAd(**self.params)
+        self.assertEqual(ClassifiedAdStatus.Inactive, x.status)
+
+    def test_given_classified_ad_with_no_text_should_not_be_published(self):
+        x = ClassifiedAd(**self.params)
+        x.set_title(ClassifiedAdTitle.create('A title'))
+        x.update_price(Price.create(
+            amount=10,
+            currency='EUR',
+            lookup=FakeCurrencyLookup
+        ))
+
+        with self.assertRaises(ValueError):
+            x.request_to_publish()
+
+        x.update_text(ClassifiedAdText.create('A Text'))
+        x.request_to_publish()
+
+    def test_given_classified_ad_with_no_title_should_not_be_published(self):
+        x = ClassifiedAd(**self.params)
+        x.update_text(ClassifiedAdText.create('A Text'))
+        x.update_price(Price.create(
+            amount=10,
+            currency='EUR',
+            lookup=FakeCurrencyLookup
+        ))
+
+        with self.assertRaises(ValueError):
+            x.request_to_publish()
+
+        x.set_title(ClassifiedAdTitle.create('A title'))
+        x.request_to_publish()
+
+    def test_given_classified_ad_with_no_price_should_not_be_published(self):
+        x = ClassifiedAd(**self.params)
+        x.update_text(ClassifiedAdText.create('A Text'))
+        x.set_title(ClassifiedAdTitle.create('A title'))
+
+        with self.assertRaises(ValueError):
+            x.request_to_publish()
+
+        x.update_price(Price.create(
+            amount=10,
+            currency='EUR',
+            lookup=FakeCurrencyLookup
+        ))
+        x.request_to_publish()
